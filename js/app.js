24f3a73a3ba89bb99c5b6cd96e6b7cc1ff8552ad
josecaos <jazzvoon@gmail.com>
2018-04-05 03:06:41 -0500

    Fixes hero screen + fondo degradado + footer nuevo + rseponsive

--- js/app.js
+++ js/app.js
@@ -2,31 +2,31 @@ $(document).foundation();
 //
 $(document).ready(function() {
 
-
   imgLiquid()
 
   // secciones
-  $("#inicio_menu").click(function(){
+  $("#inicio_menu").on('click',function() {
 
     $(".screens").hide()
+    // $(".screens").on('hide')
     $("#inicio_screen").show()
 
   })//inicio
 
-  $("#registro_menu").click(function() {
+  $("#registro_menu").on('click',function() {
 
     window.open("https://docs.google.com/forms/d/17hmdcY0_RC_Liqv8o5-7kXNlnXTScvdkr_8D2oxL234/viewform?ts=5aa73ce9");
 
   })//registro_menu
 
-  $("#programa_menu").click(function(){
+  $("#programa_menu").on('click',function(){
 
     $(".screens").hide()
     $("#programa").show()
 
   })//programa_menu
 
-  $("#contacto_menu").click(function(){
+  $("#contacto_menu").on('click',function(){
 
     $(".screens").hide()
     $("#contacto_screen").show()
