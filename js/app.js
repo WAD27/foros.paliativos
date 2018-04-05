ae912c919ef4c5ad4653993926eae775eb545518
josecaos <jazzvoon@gmail.com>
2018-04-04 23:55:03 -0500

    Reorden completo + integracion a foundation + nuevos archivos

---
+++ js/app.js
@@ -0,0 +1,52 @@
+$(document).foundation();
+//
+$( document ).ready(function() {
+
+imgLiquid()
+
+// secciones
+  $("#inicio_menu").click(function(){
+
+    $(".screens").hide()
+    $("#inicio_screen").show()
+
+  })//inicio
+
+  $("#registro_menu").click(function() {
+
+    window.open("https://docs.google.com/forms/d/17hmdcY0_RC_Liqv8o5-7kXNlnXTScvdkr_8D2oxL234/viewform?ts=5aa73ce9");
+
+  })//registro_menu
+
+  $("#programa_menu").click(function(){
+
+    $(".screens").hide()
+    $("#programa").show()
+
+  })//programa_menu
+
+  $("#contacto_menu").click(function(){
+
+    $(".screens").hide()
+    $("#contacto_screen").show()
+
+  })//contacto_menu
+
+})//
+
+function imgLiquid() {
+
+  $(".imgLiquid.imgLiquidFill").imgLiquid()
+
+  $(".imgLiquid.imgLiquidNoFill").imgLiquid({fill:false})
+
+  $(".imgLiquid.imgLiquidNoFillLeft").imgLiquid({
+    fill:false,
+    horizontalAlign:"left"
+  })
+  $(".imgLiquid.imgLiquidNoFillRight").imgLiquid({
+    fill:false,
+    horizontalAlign:"right"
+  })
+
+}
